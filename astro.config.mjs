b3a133a50273fa20fc1595683c45e35cef3f1653
Enzu83 <enzu.mc@gmail.com>
2025-07-21 22:13:02 +0200

    feat: add base config name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://Enzu83.github.io",
+	base: "/bloggie",
 	integrations: [mdx(), sitemap()],
 });
